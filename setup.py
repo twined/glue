ffbd2b13b5b61d55f0addb5276d82fc5ba95f9c7
Trond Mjøen <trond@twined.net>
2012-07-31 17:08:01 +0200

    version bump

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import find_packages
 
 setup(
     name='glue',
-    version='0.1.0',
+    version='0.1.1',
     author=u'Twined',
     author_email='www.twined.net',
     packages=find_packages(),
